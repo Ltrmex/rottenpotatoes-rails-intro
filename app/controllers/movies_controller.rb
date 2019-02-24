cdfaee0f2e1766576a94c45463fb49337d08d0dc
EC2 Default User <ec2-user@ip-172-31-17-185.eu-west-1.compute.internal>
2019-02-24 23:24:03 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,6 +24,7 @@ class MoviesController < ApplicationController
       @movies = Movie.where(rating: params[:ratings].keys)
     end
     else
+      params[:ratings] ? @movies = Movie.where(rating: params[:ratings].keys):
       @movies = Movie.all
     end
   end
